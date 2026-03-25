5830b2053ef85be7e82c49a0027a70e93e55bb8a
swzo <omarahmedmmsalem@gmail.com>
2026-03-25 19:09:13 +0200

    move mainwindow to its own class, and use Glob_Recurse to add executables in CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,9 @@
-#include <iostream>
-#include <QWidget>
-#include <QLabel>
-#include <QPushButton>
 #include <QApplication>
-#include <QVBoxLayout>
+#include "MainWindow.h"
 
 int main(int argc, char* argv[]) {
-    QApplication app(argc,argv);
-    QWidget* w = new QWidget;
-    QLabel* label = new QLabel("Study planer",w);
-    QPushButton* button = new QPushButton("click to exit", w);
-    QVBoxLayout* layout = new QVBoxLayout;
-    w->setLayout(layout);
-    layout->addWidget(label);
-    layout->addWidget(button);
-
-    QObject::connect(button, &QPushButton::clicked, [&]() {
-        app.exit();
-    });
-
-    w->show();
+    QApplication app(argc, argv);
+    MainWindow w;
+    w.show();
     return app.exec();
 }
